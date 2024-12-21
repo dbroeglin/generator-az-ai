8f9dac1f2116a96712ed9277a92401c9e230ad01
Dominique Broeglin <dominique.broeglin@gmail.com>
2024-12-21 14:29:27 +0100

    Adding ADR tools to post-create.sh

--- generators/app/templates/.devcontainer/post-create.sh
+++ generators/app/templates/.devcontainer/post-create.sh
@@ -7,3 +7,7 @@ sudo apt update
 # Install uv, see https://astral.sh for additional information
 curl -LsSf https://astral.sh/uv/install.sh | sh
 uv sync
+
+# Install adr-tools, see https://github.com/npryce/adr-tools
+git clone https://github.com/npryce/adr-tools.git --depth 1 $HOME/.adr-tools
+echo 'export PATH=$PATH:$HOME/.adr-tools/src' >> $HOME/.bashrc
