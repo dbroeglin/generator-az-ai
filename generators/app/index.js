a4fa4e662e2d5484adf0e391aad2beb7c381be3e
Dominique Broeglin <dominique.broeglin@microsoft.com>
2025-01-12 20:44:35 +0100

    Moving TIPS_AND_TRICKS.md to ./doc

--- generators/app/index.js
+++ generators/app/index.js
@@ -54,8 +54,8 @@ export default class extends Generator {
     );
 
     this.fs.copy(
-      this.templatePath("TIPS_AND_TRICKS.md"),
-      this.destinationPath("TIPS_AND_TRICKS.md")
+      this.templatePath("doc/TIPS_AND_TRICKS.md"),
+      this.destinationPath("doc/TIPS_AND_TRICKS.md")
     );
 
     this.fs.copyTpl(
