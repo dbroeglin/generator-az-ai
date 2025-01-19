254aad7dcd8cb189d871ed28823513ad99ef4aa5
Dominique Broeglin <dominique.broeglin@gmail.com>
2025-01-19 14:47:33 +0000

    Added UVm adr-tools and DevContainer CLI installation

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+[[ ${DEBUG-} =~ ^1|yes|true$ ]] && set -o xtrace
 
+# Install Yeoman and prepare the dev directory
 npm install -g yo
 npm install
 npm link
+
+# Update APT cache
+sudo apt update
+
+# Install uv, see https://astral.sh for additional information
+curl -LsSf https://astral.sh/uv/install.sh | sh
+echo 'export UV_LINK_MODE=copy' >> $HOME/.bashrc
+source $HOME/.bashrc
+
+# Install adr-tools, see https://github.com/npryce/adr-tools
+git clone https://github.com/npryce/adr-tools.git --depth 1 $HOME/.adr-tools
+echo 'export PATH=$PATH:$HOME/.adr-tools/src' >> $HOME/.bashrc
+source $HOME/.bashrc
+
+# Install devcontainers CLI (used by the test scripts)
+npm install -g @devcontainers/cli
