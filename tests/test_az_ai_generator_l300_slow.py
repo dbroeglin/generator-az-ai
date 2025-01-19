1cf7b2491e563419d8ca596b4584792917cef932
Dominique Broeglin <dominique.broeglin@gmail.com>
2025-01-19 21:18:24 +0000

    Added L300 test with authentication

--- tests/test_az_ai_generator_l300_slow.py
+++ tests/test_az_ai_generator_l300_slow.py
@@ -3,7 +3,6 @@ import pytest
 @pytest.mark.level(300)
 @pytest.mark.dependency()
 class TestAZAiGeneratorL300Slow:
-
     @pytest.mark.slow
     @pytest.mark.usefixtures("depend_on_fast", "azd_env")
     def test_deployment(self, solution):
@@ -15,3 +14,10 @@ class TestAZAiGeneratorL300Slow:
             f.write("# simple comment\n")
 
         solution.run_in("azd deploy --no-prompt")
+
+    @pytest.mark.slow
+    @pytest.mark.usefixtures("depend_on_fast", "azd_env")
+    def test_deployment_with_authentication(self, solution):
+        solution.run_in("azd env set WITH_AUTHENTICATION true")
+        solution.run_in("azd up --no-prompt")
+
